eccb38d4457a11308337f74e981e1708d6c60f8b
prppy <pojaroysg29@gmail.com>
2024-05-29 01:30:29 +0800

    django database added

---
+++ app/screens/SignUpScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View, Image, TextInput, Button } from "react-native";
+
+import colors from '../config/colors';
+
+export default function SignUpScreen(props) {
+    return (
+        <SafeAreaView>
+            <Text>signup</Text>
+        </SafeAreaView>
+    );
+}
