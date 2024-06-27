ab360001c412bdff1614dc3117868723147adeea
purp <pojaroysg29@gmail.com>
2024-06-27 17:29:58 +0800

    Resolve conflict in frontend/App.js

--- frontend/App.js
+++ frontend/App.js
@@ -5,13 +5,10 @@ import { NavigationContainer, createNavigationContainerRef } from '@react-naviga
 import { HomeStack } from './app/navigation/stacks/HomeStack';
 import { LogInStack } from './app/navigation/stacks/LogInStack';
 import { RemindersStack } from './app/navigation/stacks/RemindersStack';
-<<<<<<< Updated upstream
 import { SettingsStack } from './app/navigation/stacks/SettingsStack';
 import { ChallengesStack } from './app/navigation/stacks/ChallengesStack';
 import { Provider } from './app/components/GlobalContext';
-=======
 import { Provider } from './app/components/GlobalContext';  // Adjust the path as necessary
->>>>>>> Stashed changes
 
 export const navigationRef = createNavigationContainerRef();
 const RootStack = createStackNavigator();
@@ -24,11 +21,8 @@ export default function App() {
           <RootStack.Screen name="LogInStack" options={{ headerShown: false }} component={LogInStack} />
           <RootStack.Screen name="HomeStack" options={{ headerShown: false }} component={HomeStack} />
           <RootStack.Screen name="RemindersStack" options={{ headerShown: false }} component={RemindersStack} />
-<<<<<<< Updated upstream
           <RootStack.Screen name="ChallengesStack" options={{ headerShown: false }} component={ChallengesStack} />
           <RootStack.Screen name="SettingsStack" options={{ headerShown: false }} component={SettingsStack} />
-=======
->>>>>>> Stashed changes
         </RootStack.Navigator>
       </NavigationContainer>
     </Provider>
