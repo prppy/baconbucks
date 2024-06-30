929fddcecc1e0f31986cc01223ab932dca1a7503
purp <pojaroysg29@gmail.com>
2024-06-30 19:29:50 +0800

    i cant stop oh no-ing

--- backend/core/core/urls.py
+++ backend/core/core/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1.0/user/', include('userAPI.urls')),
     path('api/v1.0/app/', include('appSettings.urls')),
-    path('api/v1.0/log/spend/', include('logAPI.urls'))
+    path('api/v1.0/log/', include('logAPI.urls'))
 ]
 
 # localhost:8000/admin/
